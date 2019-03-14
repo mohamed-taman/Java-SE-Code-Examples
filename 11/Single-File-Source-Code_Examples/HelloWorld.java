d464516603be0522168710d04e03aad216d972fd
Mohamed Mahmoud Taman <mohamed.taman@gmail.com>
2019-03-14 09:42:46 +0100

    Change IBM DeveloperWork to IBM Developer

--- 11/Single-File-Source-Code_Examples/HelloWorld.java
+++ 11/Single-File-Source-Code_Examples/HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello IBM DeveloperWorks World");
+		System.out.println("Hello IBM Developer World");
 	}
 }
